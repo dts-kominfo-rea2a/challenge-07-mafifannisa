27df9661e51b0104fdfc10fa7909e09a09617059
Moch. Afif <mafifannisa@gmail.com>
2022-06-11 05:47:07 +0700

    Upload Challenge - Javascript Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (esmosi)=>{
+  try {
+    const jumlahEsmosi = (await promiseTheaterIXX()).concat(await promiseTheaterVGC());
+    const hasilEsmosi = jumlahEsmosi.filter(element => (element.hasil === esmosi));
+    return hasilEsmosi.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
